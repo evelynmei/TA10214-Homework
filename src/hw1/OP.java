b33dd23a2ab2640cffe298978fb2c4c0386eaea9
Evelyn Mei <yumei927@gmail.com>
2024-04-21 16:07:39 +0800

    First commit

---
+++ src/hw1/OP.java
@@ -0,0 +1,61 @@
+package hw1;
+
+public class OP {
+	public static void main(String[] args) {
+
+		// 請設計一隻Java程式,計算12,6這兩個數值的和與積
+		int sum = 12 + 6;
+		int product = 12 * 6;
+		System.out.println("和:" + sum + '\n' + "積:" + product);
+		System.out.println("===============");
+
+		// 請設計一隻Java程式,計算200顆蛋共是幾打幾顆? (一打為12顆)
+		int num1 = 200 / 12;
+		int num2 = 200 % 12;
+		System.out.println(num1 + "打" + num2 + "顆");
+		System.out.println("===============");
+
+		// 請由程式算出256559秒為多少天、多少小時、多少分與多少秒
+		int totalS = 256559;
+		int sPerDay = 24 * 60 * 60;
+		int day = totalS / sPerDay;
+		int leftS = totalS % sPerDay;
+		int sPerHour = 60 * 60;
+		int hour = leftS / sPerHour;
+		int leftM = leftS % sPerHour;
+		int sPerM = 60;
+		int minute = leftM / sPerM;
+		int second = leftM % sPerM;
+		System.out.printf("%d天 %d時 %d分 %d秒%n", day, hour, minute, second);
+		System.out.println("===============");
+
+//		int total = 256559;
+//		int dayPrime = total / 24 / 60 / 60 ;
+//		int hourPrime = ( total % ( 24 * 60 * 60) ) / (60 * 60);
+//		int minutePrime = ( total % (24 * 60 * 60) % ( 60 * 60) ) / 60;
+//		int secondPrime = ( ( total % (24 * 60 * 60) % ( 60 * 60) ) ) % 60;
+//		System.out.printf( "%d天 %d時 %d分 %d秒%n", dayPrime, hourPrime, minutePrime, secondPrime);
+		// 請定義一個常數為3.1415(圓周率),並計算半徑為5的圓面積(r^2*pi)與圓周長(2r*pi)
+		final double PI = 3.1415;
+		double area = 5 * 5 * PI;
+		double circumference = 2 * 5 * PI;
+		System.out.printf("圓面積:%.4f 圓周長:%.4f%n", area, circumference);
+		System.out.println("===============");
+
+		// 某人在銀行存入150萬,銀行利率為2%,如果每年利息都繼續存入銀行,請用程式計算10年後,本金加利息共有多少錢(用複利計算,公式: 本利和=
+		// 本金*(1+利率)^2)
+		 1500000 * (1 + 0.02)
+
+		/*
+		 * 請寫一隻程式,利用System.out.println()印出以下三個運算式結果: 5 + 5 5 + ‘5’ 5 + “5”
+		 * 並請用註解各別說明答案的產生原因
+		 */
+		// 數學加法結果
+		System.out.println(5 + 5);
+		// '5'表示為char, 但與前面的int 5相加時, 較小的char晉升為int, 在ASCII中char 5的int為53, 故5+53=58
+		System.out.println(5 + '5');
+		// int 5串接string 5, 較小的int 5晉升為string
+		System.out.println(5 + "5");
+	}
+
+}
