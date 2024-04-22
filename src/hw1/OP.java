a3ce022508784c52e52df84962d6fd8f877e09ac
Evelyn Mei <yumei927@gmail.com>
2024-04-23 02:46:45 +0800

    complete OP.java

--- src/hw1/OP.java
+++ src/hw1/OP.java
@@ -29,12 +29,6 @@ public class OP {
 		System.out.printf("%d天 %d時 %d分 %d秒%n", day, hour, minute, second);
 		System.out.println("===============");
 
-//		int total = 256559;
-//		int dayPrime = total / 24 / 60 / 60 ;
-//		int hourPrime = ( total % ( 24 * 60 * 60) ) / (60 * 60);
-//		int minutePrime = ( total % (24 * 60 * 60) % ( 60 * 60) ) / 60;
-//		int secondPrime = ( ( total % (24 * 60 * 60) % ( 60 * 60) ) ) % 60;
-//		System.out.printf( "%d天 %d時 %d分 %d秒%n", dayPrime, hourPrime, minutePrime, secondPrime);
 		// 請定義一個常數為3.1415(圓周率),並計算半徑為5的圓面積(r^2*pi)與圓周長(2r*pi)
 		final double PI = 3.1415;
 		double area = 5 * 5 * PI;
@@ -42,19 +36,24 @@ public class OP {
 		System.out.printf("圓面積:%.4f 圓周長:%.4f%n", area, circumference);
 		System.out.println("===============");
 
-		// 某人在銀行存入150萬,銀行利率為2%,如果每年利息都繼續存入銀行,請用程式計算10年後,本金加利息共有多少錢(用複利計算,公式: 本利和=
-		// 本金*(1+利率)^2)
-		 1500000 * (1 + 0.02)
+		// 某人在銀行存入150萬,銀行利率為2%,如果每年利息都繼續存入銀行,請用程式計算10年後,本金加利息共有多少錢
+		// (用複利計算,公式: 本利和=本金*(1+利率)^期數
+		int p = 1500000;
+		int n = 10;
+		double i = 0.02;
+		double s = p * Math.pow(1 + i, n);
+		System.out.println("本利和: " + Math.round(s));
+		System.out.println("===============");
 
 		/*
-		 * 請寫一隻程式,利用System.out.println()印出以下三個運算式結果: 5 + 5 5 + ‘5’ 5 + “5”
+		 * 請寫一隻程式,利用System.out.println()印出以下三個運算式結果: 5 + 5, 5 + ‘5’, 5 + “5”
 		 * 並請用註解各別說明答案的產生原因
 		 */
-		// 數學加法結果
+		// 數學加法結果, 5+5=10
 		System.out.println(5 + 5);
-		// '5'表示為char, 但與前面的int 5相加時, 較小的char晉升為int, 在ASCII中char 5的int為53, 故5+53=58
+		// '5'為char, 但與前面的int 5相加時, 位元較小的char晉升為int, 在ASCII中char 5的int值為53, 故5+53=58
 		System.out.println(5 + '5');
-		// int 5串接string 5, 較小的int 5晉升為string
+		// int 5串接string 5, 位元較小的int 5晉升為string, 串接的結果為55
 		System.out.println(5 + "5");
 	}
 
